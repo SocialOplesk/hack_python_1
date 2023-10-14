9f94788b959672afbc10ee57eeddb7db0f151209
Alfredo Rodriguez <alfredobose@gmail.com>
2023-10-14 12:29:07 -0400

    feat:hack10

--- hack_4.py
+++ hack_4.py
@@ -4,5 +4,5 @@ text: "fooziman" output => "foozimaN"
 
 def fn_hack_4():
     result = "fooziman"
-    #...
+    result = result[:7] + result[-1].upper()
     return result
--- hack_6.py
+++ hack_6.py
@@ -4,5 +4,7 @@ loop: for [] output => [0,1,2,3,4,5]
 
 def fn_hack_6():
     result = []
-    #...
+    for i in [1,2,3,4,5]:
+        result
+
     return result  
